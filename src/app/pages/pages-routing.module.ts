ab3d5e40208ef6312cdef8a23bc1f7a6d135ef85
Anamaria Miranda <miranda_anam@hotmail.com>
2020-12-02 00:33:41 -0500

    feat: se retira direccionamiento por defecto a aprobaciones #10

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -22,16 +22,6 @@ const routes: Routes = [
       path: 'prueba',
       loadChildren: () => import('./primer-modulo/primer-modulo.module')
       .then(m => m.PrimerModuloModule),
-    },
-    {
-      path: '',
-      redirectTo: 'aprobaciones',
-      pathMatch: 'full',
-    },
-    {
-      path: '**',
-      redirectTo: 'aprobaciones',
-      pathMatch: 'full',
     }]
   }
 ];
