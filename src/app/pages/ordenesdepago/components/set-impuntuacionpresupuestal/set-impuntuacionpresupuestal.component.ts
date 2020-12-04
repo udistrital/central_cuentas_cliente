ca42c3251d1863e343e2d633c0f742792a0a4570
Jeisson P <jeisson005@hotmail.com>
2020-12-04 16:26:46 -0500

    Contenido del paso 3 para creacion de ordenes de pago #16

--- src/app/pages/ordenesdepago/components/set-impuntuacionpresupuestal/set-impuntuacionpresupuestal.component.ts
+++ src/app/pages/ordenesdepago/components/set-impuntuacionpresupuestal/set-impuntuacionpresupuestal.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
+import { 
+  CONFIGURACION_HISTORIAL, DATOS_HISTORIAL,
+  CONFIGURACION_IMPUNTUACION, DATOS_IMPUNTUACION
+ } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'ngx-set-impuntuacionpresupuestal',
@@ -8,8 +12,17 @@ import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 })
 export class SetImpuntuacionpresupuestalComponent implements OnInit {
   impuntuacionPresupuestal: FormGroup;
+  configTableHistorial: any;
+  datosTableHistorial: any;
+  configTableImpuntuacion: any;
+  datosTableImpuntuacion: any;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.configTableHistorial = CONFIGURACION_HISTORIAL;
+    this.datosTableHistorial = DATOS_HISTORIAL;
+    this.configTableImpuntuacion = CONFIGURACION_IMPUNTUACION;
+    this.datosTableImpuntuacion = DATOS_IMPUNTUACION;
+  }
 
   ngOnInit() {
     this.impuntuacionPresupuestal = this.fb.group({
