2412029f741b7729bc61e58bbd59ee9f8de130f2
Anamaria Miranda <miranda_anam@hotmail.com>
2020-12-15 22:07:07 -0500

    se corrigen espacios en blanco

--- src/app/pages/anulaciones/components/create-anulation/create-anulation.component.ts
+++ src/app/pages/anulaciones/components/create-anulation/create-anulation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DATOS_ORDENPAGO, DATOS_RELACION_AUTORIZACION } from '../../interfaces/interfaces'
+import { DATOS_ORDENPAGO, DATOS_RELACION_AUTORIZACION } from '../../interfaces/interfaces';
 import { FormService } from '../../../../shared/services/form.service';
 @Component({
   selector: 'ngx-create-anulation',
@@ -8,19 +8,19 @@ import { FormService } from '../../../../shared/services/form.service';
 })
 export class CreateAnulationComponent implements OnInit {
 
-  tipoAnulacion:any;
+  tipoAnulacion: any;
   datosOrden: any;
   datosRelacion: any;
 
   titlesOrden: String[] = ['Vigencia', 'Consecutivo', 'Tipo ID', 'No. ID', 'Nombre'];
-  attributesOrden: any[] = [['vigencia'], ['consecutivo'], ['tipoID'], ['numeroID'],['nombre']];
+  attributesOrden: any[] = [['vigencia'], ['consecutivo'], ['tipoID'], ['numeroID'], ['nombre']];
 
   titlesRelacion: String[] = ['Área funcional', 'Vigencia', 'Mes', 'Consecutivo', 'Estado'];
   attributesRelacion: any[] = [['areaFuncional'], ['vigencia'], ['mes'], ['consecutivo'], ['estado']];
 
   constructor(
     public form: FormService,
-  ) { 
+  ) {
     this.datosOrden = DATOS_ORDENPAGO;
     this.datosRelacion = DATOS_RELACION_AUTORIZACION;
   }
@@ -30,8 +30,8 @@ export class CreateAnulationComponent implements OnInit {
     this.form.aprobacionesElegidas = [];
   }
 
-  eleccionAnulacion(data:any){
-    this.tipoAnulacion = data.tipoAnulacion;  
+  eleccionAnulacion( data: any ) {
+    this.tipoAnulacion = data.tipoAnulacion;
   }
 
 }
