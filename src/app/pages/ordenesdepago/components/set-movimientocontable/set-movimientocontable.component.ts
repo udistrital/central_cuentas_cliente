0c7443b72c71aff8292cc6dcddcd13a715a1beb3
Jeisson P <jeisson005@hotmail.com>
2020-12-16 23:42:19 -0500

    fix: Ajustes para pull #17

--- src/app/pages/ordenesdepago/components/set-movimientocontable/set-movimientocontable.component.ts
+++ src/app/pages/ordenesdepago/components/set-movimientocontable/set-movimientocontable.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 import {
   CONFIGURACION_MOVIMIENTO_CONTABLE, DATOS_MOVIMIENTO_CONTABLE
  } from '../../interfaces/interfaces';
+import { ElementRef } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { getFilaSeleccionada } from '../../../../shared/selectors/shared.selectors';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngx-set-movimientocontable',
@@ -10,18 +14,54 @@ import {
   styleUrls: ['./set-movimientocontable.component.scss']
 })
 export class SetMovimientocontableComponent implements OnInit {
+  @ViewChild('eliminarModal', { static: false }) eliminarModal: ElementRef;
   movimientoContable: FormGroup;
   configTableMovimientoContable: any;
   datosTableMovimientoContable: any;
+  subscription: any;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private modalService: NgbModal, private store: Store<any>) {
     this.configTableMovimientoContable = CONFIGURACION_MOVIMIENTO_CONTABLE;
-    this.datosTableMovimientoContable = DATOS_MOVIMIENTO_CONTABLE;
+    this.datosTableMovimientoContable = [];
   }
 
   ngOnInit() {
     this.movimientoContable = this.fb.group({
       firstCtrl: ['', Validators.required]
     });
+    this.agregar(); // TODO
+    this.subscription = this.store.select(getFilaSeleccionada).subscribe((accion) => {
+      if (accion) {
+        if (accion.accion.idStep === 4 && accion.accion.name === 'modificar') {
+          this.modalEliminar(accion.fila);
+        }
+      }
+    });
+  }
+
+  modalEliminar(fila: any) {
+    this.modalService.open(this.eliminarModal).result.then((result) => {
+      if (`${result}`) {
+        this.datosTableMovimientoContable.splice(this.datosTableMovimientoContable.findIndex(
+          (element: any) => element.codigo === fila.codigo
+          && element.descuento === fila.descuento
+          && element.base === fila.base
+          && element.valor === fila.valor
+          ), 1);
+      }
+    });
+  }
+
+  agregar() {
+    // TODO
+    this.datosTableMovimientoContable.push(DATOS_MOVIMIENTO_CONTABLE[0]);
+  }
+
+  valorNeto() {
+    return this.datosTableMovimientoContable.reduce((a: any, b: { valor: number; }) => a + b.valor, 0);
+  }
+
+  valorTotal() {
+    return this.datosTableMovimientoContable.reduce((a: any, b: { valor: number; }) => a + b.valor, 0);
   }
 }
