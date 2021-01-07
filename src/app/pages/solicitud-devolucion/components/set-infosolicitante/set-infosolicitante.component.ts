bdf01062ac18a8c747a7eb20c58868535f6a2cac
Jeisson P <jeisson005@hotmail.com>
2021-01-06 21:44:44 -0500

    feat: Se consumen conceptos con virtual scroll para solicitud de devolución #97

--- src/app/pages/solicitud-devolucion/components/set-infosolicitante/set-infosolicitante.component.ts
+++ src/app/pages/solicitud-devolucion/components/set-infosolicitante/set-infosolicitante.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { GetConceptosContables } from '../../../../shared/actions/shared.actions';
+import { getConceptosContables } from '../../../../shared/selectors/shared.selectors';
 
 @Component({
   selector: 'ngx-set-infosolicitante',
   templateUrl: './set-infosolicitante.component.html',
   styleUrls: ['./set-infosolicitante.component.scss']
 })
-export class SetInfosolicitanteComponent implements OnInit {
+export class SetInfosolicitanteComponent implements OnInit, OnDestroy {
   datosSolicitante: FormGroup;
+  subscriptionConceptos$: any;
+  conceptosContables: any;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private store: Store<any>) {
+    this.conceptosContables = [];
+    this.store.dispatch(GetConceptosContables({}));
+  }
 
   ngOnInit() {
     this.crearFormulario();
+    // Conceptos contables
+    this.subscriptionConceptos$ = this.store.select(getConceptosContables).subscribe((accion) => {
+      if (accion && accion[0]) this.conceptosContables = accion[0];
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscriptionConceptos$.unsubscribe();
   }
 
   crearFormulario() {
--- src/app/pages/solicitud-devolucion/solicitud-devolucion.module.ts
+++ src/app/pages/solicitud-devolucion/solicitud-devolucion.module.ts
@@ -6,7 +6,7 @@ import { TableSolicitudesDevolucionComponent } from './components/table-solicitu
 import { CrearSolicitudesDevolucionComponent } from './components/crear-solicitudes-devolucion/crear-solicitudes-devolucion.component';
 import { SharedModule } from '../../shared/shared.module';
 import { SetInfosolicitudComponent } from './components/set-infosolicitud/set-infosolicitud.component';
-import { MatStepperModule, MatTabsModule } from '@angular/material';
+import { MatStepperModule, MatTabsModule, MatSelectModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SetInfosolicitanteComponent } from './components/set-infosolicitante/set-infosolicitante.component';
@@ -16,6 +16,7 @@ import * as fromSolicitudDevolucion from './reducers/solicitud-devolucion.reduce
 import { EffectsModule } from '@ngrx/effects';
 import { SolicitudDevolucionEffects } from './effects/solicitud-devolucion.effects';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 
 @NgModule({
@@ -31,7 +32,9 @@ import { CurrencyMaskModule } from 'ng2-currency-mask';
     MatTabsModule,
     StoreModule.forFeature(fromSolicitudDevolucion.solicitudDevolucionFeatureKey, fromSolicitudDevolucion.reducer),
     EffectsModule.forFeature([SolicitudDevolucionEffects]),
-    CurrencyMaskModule
+    CurrencyMaskModule,
+    MatSelectModule,
+    ScrollingModule
   ]
 })
 export class SolicitudDevolucionModule { }
