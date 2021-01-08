1ab5eca046de7584ce4244b3433754ad0f38803d
Jeisson P <jeisson005@hotmail.com>
2021-01-07 22:27:22 -0500

    fix: Ajuste pull #96

--- src/app/pages/ordenesdepago/components/set-datoscompromiso/set-datoscompromiso.component.ts
+++ src/app/pages/ordenesdepago/components/set-datoscompromiso/set-datoscompromiso.component.ts
@@ -50,7 +50,6 @@ export class SetDatoscompromisoComponent implements OnInit, OnDestroy {
         Validators.pattern('^[0-9]*$')
       ]],
       vigencia: ['', Validators.required],
-      tipoOrden: ['', Validators.required],
     });
   }
 
