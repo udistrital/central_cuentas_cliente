1d684f0d7d9c79a326d818d3a1afbd2bdc5ff74b
Diego Gutierrez <diagutierrezro@unal.edu.co>
2022-04-25 11:10:35 -0500

    fix: Correccion de variables de entorno

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -19,7 +19,7 @@ export const environment = {
     DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
     GESTOR_DOCUMENTAL_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
     CONSECUTIVOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/consecutivos_crud/v1/',
-    CENTRAL_CUENTAS_CRUD_SERVICE: 'http://localhost:3000/',
+    CENTRAL_CUENTAS_CRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/central_cuentas_crud/v1/',
     ADMINISTRATIVA_AMAZON_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1/',
     MOVIMIENTOS_ARKA_CRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/movimientos_arka_crud/v1/',
     PLAN_ADQUISICIONES_CRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_adquisiciones_crud/v1/',
